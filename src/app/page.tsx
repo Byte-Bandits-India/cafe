2be05076c370bafa5790542c2e4eb8830af33dc5
abraham-r-bytebandits <abraham.r@bytebandits.in>
2026-04-02 15:26:46 +0530

    checking the dev server

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,5 @@
 export default function Home() {
   return (
-    <h1 className="text-3xl font-bold underline">Home</h1>
+    <h1 className="text-3xl font-bold underline">Development server</h1>
   );
 }
